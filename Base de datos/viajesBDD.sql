23c3e6a27802ac3fe764a4661aae180bfd821fdf
the97prime <oscar.penaherrera@epn.edu.ec>
2019-07-02 17:49:59 -0500

    Cambios en base de Datos

--- Base de datos/viajesBDD.sql
+++ Base de datos/viajesBDD.sql
@@ -3,7 +3,6 @@
 -- Host: localhost    Database: viajesbdd
 -- ------------------------------------------------------
 -- Server version	8.0.14
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
