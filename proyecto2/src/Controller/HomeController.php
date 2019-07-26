f5e9ea4b7ee32a7688c1a015f1fdb7c9f6ae36a8
Elvis <elvis.perez01@epn.edu.ec>
2019-07-25 23:38:50 -0500

    Creacion de form en el header v2

--- proyecto2/src/Controller/HomeController.php
+++ proyecto2/src/Controller/HomeController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Form\HeadType;
+use App\Form\SearchBusType;
+use App\Form\SearchHotelType;
+use App\Form\SearchPaqueteType;
+
 
 class HomeController extends AbstractController
 {
@@ -13,9 +16,13 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        $form = $this->createForm(HeadType::class,null);
+        $form1 = $this->createForm(SearchBusType::class,null);
+        $form2 = $this->createForm(SearchHotelType::class,null);
+        $form3 = $this->createForm(SearchPaqueteType::class,null);
         return $this->render('home/index.html.twig', [
-            'form'=>$form->createView()
+            'form1'=>$form1->createView(),
+            'form2'=>$form2->createView(),
+            'form3'=>$form3->createView()
         ]);
     }
 }
